6aa73183b22997c0bb0ecc359e3dd22b075728ed
Veselin Pizurica <veselinpizurica@Veselins-MacBook-Pro.local>
2024-10-07 14:40:31 +0200

    used sdk to run the bot

--- js/aiBot.js
+++ js/aiBot.js
@@ -45,30 +45,20 @@ class GenAIBot {
     async runBot(question) {
       console.log('runBot', question);
       this.lastQuestion = question;
-      const url = `${this.client.gateway}/rules/v1/templates/${this.template}/run`;
-      let trun;
-      try {
-        trun = await axios.post(url, {
-          variables: {
-            question,
-            messages: this.fullReply,
-            model: this.AIModel
-          }
-        }, {
-          headers: {
-            'Authorization': `Bearer ${this.client.token}`
-          }
-        });
-      } catch (error) {
-        throw new Error("error running bot", error);
-      }
-      if(trun.data === undefined) {
+      let ret
+      try{
+        ret = await client.templates.run(this.template, { variables: { question, messages: this.fullReply,  model: this.AIModel}})
+      } catch(error){
         throw new Error("error running the bot, template failed to run", error);
-      } else if(trun.data.taskOutput === undefined) {
+      }
+      
+      if(ret === undefined) {
+        throw new Error("error running the bot, template failed to respond", error);
+      } else if(ret.taskOutput === undefined) {
         this.reset()
         throw new Error("error running the bot, I will reset the cache, please try again", error);
       }
-      const response = trun.data.taskOutput;
+      const response = ret.taskOutput;
       if(response.messages.length > 1) {
         this.fullReply = response.messages;
         this.lastReplyMessage = this._extractMessageText(response.messages[response.messages.length - 1])
