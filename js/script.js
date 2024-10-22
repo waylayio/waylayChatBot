ff21cf49ff22457eb17aacc7977dea962da109f4
Veselin Pizurica <veselinpizurica@Veselins-MacBook-Pro.local>
2024-10-22 10:55:28 +0200

    merging all themse in one style

--- js/script.js
+++ js/script.js
@@ -186,9 +186,9 @@ const loadDataFromLocalstorage = () => {
 
   document.body.classList.toggle("light-mode", themeColor === "light_mode");
   themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
-  const markdownBodies = document.querySelectorAll('.markdown-body');
-  markdownBodies.forEach(element => 
-    element.setAttribute('data-theme', document.body.classList.contains("light-mode") ? "light" : "dark") )
+  // const markdownBodies = document.querySelectorAll('.markdown-body');
+  // markdownBodies.forEach(element => 
+  //   element.setAttribute('data-theme', document.body.classList.contains("light-mode") ? "light" : "dark") )
 
   const defaultText = `<div class="squeeze-and-disappear default-text">
                             <h1>Waylay iBot</h1>
@@ -204,9 +204,9 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("themeColor", themeButton.innerText);
   themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
 
-  const markdownBodies = document.querySelectorAll('.markdown-body');
-  markdownBodies.forEach(element => 
-    element.setAttribute('data-theme', document.body.classList.contains("light-mode") ? "light" : "dark") )
+  // const markdownBodies = document.querySelectorAll('.markdown-body');
+  // markdownBodies.forEach(element => 
+  //   element.setAttribute('data-theme', document.body.classList.contains("light-mode") ? "light" : "dark") )
 });
 
 const createChatElement = (content, className) => {
