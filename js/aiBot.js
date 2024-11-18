2e72833cb3b88fbc61002e0d158cb96a24f79daf
Veselin Pizurica <veselinpizurica@Veselins-MacBook-Pro.local>
2024-11-18 15:39:42 +0100

    update sessiom id creation

--- js/aiBot.js
+++ js/aiBot.js
@@ -12,10 +12,10 @@ class GenAIBot {
     }
 
      generateUUID(template) {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      return template + "_" + 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
           const r = Math.random() * 16 | 0;
           const v = c === 'x' ? r : (r & 0x3 | 0x8);
-          return template + "_" + v.toString(16);
+          v.toString(16);
       })
     }
 
